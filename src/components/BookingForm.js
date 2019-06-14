3eb375398b41e7b73929a10194b47625a7dacb95
JewelBreeveld <rjbreeveld@gmail.com>
2019-06-14 14:38:00 +0200

    Required fields cannot be submitted if empty

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -7,21 +7,21 @@ class BookingForm extends Component {
     return (
       <form>
         {/*1. pickup => inputfield*/}
-        <input type="text" name="name" placeholder="Address" />
+        <input type="text" name="name" placeholder="Address" required />
         {/*2. via => inputfield*/}
         <input type="text" name="name" placeholder="Address" />
         {/* dropoff => inputfield */}
-        <input type="text" name="name" placeholder="Address" />
+        <input type="text" name="name" placeholder="Address" required />
         {/* ASAP or Date/Time Component */}
         <PickupTime />
         {/* Vehicle Component */}
         <ChooseVehicle />
         {/* Name passenger => inputfield */}
-        <input type="text" name="name" placeholder="Full name" />
+        <input type="text" name="name" placeholder="Full name" required />
         {/* Phone passenger => inputfield */}
-        <input type="text" name="name" placeholder="Phone-number" />
+        <input type="text" name="name" placeholder="Phone-number" required />
         {/* Email passenger */}
-        <input type="text" name="name" placeholder="E-mail" />
+        <input type="text" name="name" placeholder="E-mail" required />
         {/* Submit form button*/}
         <input type="submit" value="Submit" />
       </form>
